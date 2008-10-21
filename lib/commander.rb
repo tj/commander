89bb0048a8e30094bf5470936413a5b1842ae394
TJ Holowaychuk <tj@vision-media.ca>
2008-10-21 15:46:11 -0700

    - Abstracted files, functionality remains the same

--- lib/commander.rb
+++ lib/commander.rb
@@ -1,16 +1,9 @@
 
 $:.unshift(File.expand_path(File.dirname(__FILE__)))
 
-module Commander
-  MAJOR = 1
-  MINOR = 0
-  TINY = 0
-  VERSION = [MAJOR, MINOR, TINY].join('.')
-end
+require 'commander/commander'
 
 class Object
   include Commander
 end
-
-require 'commander/commander'
 
