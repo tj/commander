b94a43cc77c42785e9072905d7fc3bd484da5de1
TJ Holowaychuk <tj@vision-media.ca>
2008-11-22 13:44:28 -0800

    - Added helper to all spec files so they can be run on their own...

--- spec/all_spec.rb
+++ spec/all_spec.rb
@@ -1,6 +1,4 @@
 
-require File.expand_path(File.dirname(__FILE__) + '/../lib/commander')
-
 Dir['./spec/**/*.rb'].each do |file|
   require file unless file == './spec/all_spec.rb'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 
+require File.expand_path(File.dirname(__FILE__) + '/../lib/commander')
 require 'stringio'
 
 def create_test_command
