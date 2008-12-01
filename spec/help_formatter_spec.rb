ee71a20d632cdce144b4bf174a49f728a3c5dd3b
TJ Holowaychuk <tj@vision-media.ca>
2008-12-01 08:37:28 -0800

    - Added sped for displaying help using the --help switch

--- spec/help_formatter_spec.rb
+++ spec/help_formatter_spec.rb
@@ -1,6 +1,13 @@
 
 describe Commander::HelpFormatter do
   
+  it "should display global help using --help switch" do
+    intput, output = new_command_runner '--help'
+    program :help_formatter, Commander::HelpFormatter::Base
+    command_runner.run!
+    output.string.should eql("Implement global help here\n")
+  end
+  
   it "should display global help using help command" do
     input, output = new_command_runner 'help'
     program :help_formatter, Commander::HelpFormatter::Base
