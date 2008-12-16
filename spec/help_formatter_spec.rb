e2ff66f5c665f76192efbbdb57bced3160c216bb
TJ Holowaychuk <tj@vision-media.ca>
2008-12-16 13:56:14 -0800

    - Fixed formatter spec to reflect change from puts to say

--- spec/help_formatter_spec.rb
+++ spec/help_formatter_spec.rb
@@ -1,25 +1,31 @@
 
 describe Commander::HelpFormatter do
   
+  before :each do
+    @input = StringIO.new
+    @output = StringIO.new
+    $terminal = HighLine.new @input, @output
+  end
+  
   it "should display global help using --help switch" do
-    intput, output = new_command_runner '--help'
+    new_command_runner '--help'
     program :help_formatter, Commander::HelpFormatter::Base
     command_runner.run!
-    output.string.should eql("Implement global help here\n")
+    @output.string.should eql("Implement global help here\n")
   end
   
   it "should display global help using help command" do
-    input, output = new_command_runner 'help'
+    new_command_runner 'help'
     program :help_formatter, Commander::HelpFormatter::Base
     command_runner.run!
-    output.string.should eql("Implement global help here\n")
+    @output.string.should eql("Implement global help here\n")
   end
   
   it "should display command help" do
-    input, output = new_command_runner 'help', 'test'
+    new_command_runner 'help', 'test'
     program :help_formatter, Commander::HelpFormatter::Base
     command_runner.run!
-    output.string.should eql("Implement help for test here\n")
+    @output.string.should eql("Implement help for test here\n")
   end
       
 end
