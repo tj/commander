cf729d74d0997b3c1bb4129378f1355ae58c435f
TJ Holowaychuk <tj@vision-media.ca>
2009-01-15 18:54:45 -0800

    - Added core ext spec

---
+++ spec/core_ext_spec.rb
@@ -0,0 +1,9 @@
+
+describe Commander do
+  describe String do
+    it "should tokenize strings" do
+      s = 'Welcome :name, enjoy your :object'.tokenize!({ :name => 'TJ', :object => 'cookie' })
+      s.should == 'Welcome TJ, enjoy your cookie'
+    end
+  end
+end
