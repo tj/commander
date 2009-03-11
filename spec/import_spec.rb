9d1f24e4599fe667b96e793e50af81c135bced3f
benedikt <benedikt@synatic.net>
2009-03-11 16:52:13 +0800

    Fixed Kernel#method_missing to preserve its original behavior (Raise errors for missing methods or variables)
    Signed-off-by: TJ Holowaychuk <tj@vision-media.ca>

---
+++ spec/import_spec.rb
@@ -0,0 +1,13 @@
+describe Kernel do
+  
+  describe "#method_missing" do
+    it "should preserve its original behavior for missing methods" do
+      lambda { i_am_a_missing_method() }.should raise_error NoMethodError
+    end
+    
+    it "should preserve its original behavior for missing variables" do
+      lambda { i_am_a_missing_variable }.should raise_error NameError
+    end
+  end
+  
+end
