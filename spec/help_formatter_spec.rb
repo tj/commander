37d0a0ab83c9ed39f5612bd4d5de898e46f9d63f
TJ Holowaychuk <tj@vision-media.ca>
2009-03-11 16:57:32 -0700

    Refactored formatters spec

--- spec/help_formatter_spec.rb
+++ spec/help_formatter_spec.rb
@@ -7,25 +7,23 @@ describe Commander::HelpFormatter do
     $terminal = HighLine.new @input, @output
   end
   
-  it "should display global help using --help switch" do
-    new_command_runner '--help'
+  def run *args
+    new_command_runner *args
     program :help_formatter, Commander::HelpFormatter::Base
-    command_runner.run!
-    @output.string.should eql("Implement global help here\n")
+    command_runner.run!    
+    @output.string
+  end
+  
+  it "should display global help using --help switch" do
+    run('--help').should == "Implement global help here\n"
   end
   
   it "should display global help using help command" do
-    new_command_runner 'help'
-    program :help_formatter, Commander::HelpFormatter::Base
-    command_runner.run!
-    @output.string.should eql("Implement global help here\n")
+    run('help').should == "Implement global help here\n"
   end
   
   it "should display command help" do
-    new_command_runner 'help', 'test'
-    program :help_formatter, Commander::HelpFormatter::Base
-    command_runner.run!
-    @output.string.should eql("Implement help for test here\n")
+    run('help', 'test').should == "Implement help for test here\n"
   end
       
 end
