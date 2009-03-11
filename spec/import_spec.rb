e89deb97e7e99c077ff4bf8250e9f608504ecfa9
TJ Holowaychuk <tj@vision-media.ca>
2009-03-11 08:31:58 -0700

    Fix paren warnings in import spec

--- spec/import_spec.rb
+++ spec/import_spec.rb
@@ -2,11 +2,11 @@ describe Kernel do
   
   describe "#method_missing" do
     it "should preserve its original behavior for missing methods" do
-      lambda { i_am_a_missing_method() }.should raise_error NoMethodError
+      lambda { i_am_a_missing_method() }.should raise_error(NoMethodError)
     end
     
     it "should preserve its original behavior for missing variables" do
-      lambda { i_am_a_missing_variable }.should raise_error NameError
+      lambda { i_am_a_missing_variable }.should raise_error(NameError)
     end
   end
   
