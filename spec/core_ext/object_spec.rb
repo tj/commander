9d13d5d3ca5c9513cd11d1e431b51db5811cb9f6
TJ Holowaychuk <tj@vision-media.ca>
2009-03-11 22:13:25 -0700

    Moved method missing spec to object_spec.rb

--- spec/core_ext/object_spec.rb
+++ spec/core_ext/object_spec.rb
@@ -6,5 +6,15 @@ describe Object do
       lambda {}.get_binding.should be_instance_of(Binding)
     end
   end
-  
+
+  describe "#method_missing" do
+    it "should preserve its original behavior for missing methods" do
+      lambda { i_am_a_missing_method() }.should raise_error(NoMethodError)
+    end
+
+    it "should preserve its original behavior for missing variables" do
+      lambda { i_am_a_missing_variable }.should raise_error(NameError)
+    end
+  end
+
 end
