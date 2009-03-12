b633ac4a9750b291507e9880950e505655b19621
TJ Holowaychuk <tj@vision-media.ca>
2009-03-11 20:20:23 -0700

    Refactored Array#parse using evaluation of %w( array alternative syntax
    This approach is nearly twice as fast as the previous scan method, and since the Ruby regexp engine does not currently
    support negative lookbehinds its a little verbose

--- lib/commander/core_ext/array.rb
+++ lib/commander/core_ext/array.rb
@@ -13,7 +13,8 @@ class Array
   #
   
   def self.parse string
-    string.scan(/(?:\w|\d|\\ )+/).map{ |value| value.sub('\\', '')  }
+    #string.scan(/(?:[\w\d]|\\ )+/).map{ |value| value.sub('\\', '')  }
+    eval "%w(#{string})"
   end
   
   ##
