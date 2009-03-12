b665f317467034620e2734015564e6f0150dfee8
TJ Holowaychuk <tj@vision-media.ca>
2009-03-11 22:53:20 -0700

    Less ugly autoloading

--- lib/commander/help_formatters.rb
+++ lib/commander/help_formatters.rb
@@ -1,6 +1,8 @@
 
 require 'commander/help_formatters/base'
 
-module Commander::HelpFormatter
-  autoload :Terminal, 'commander/help_formatters/terminal'
+module Commander
+  module HelpFormatter
+    autoload :Terminal, 'commander/help_formatters/terminal'
+  end
 end
