7493d04236658cccbeb3375b5dce030e13eecf70
TJ Holowaychuk <tj@vision-media.ca>
2009-03-20 09:16:53 -0700

    Removed some specs due to the blank options struct not having methods required for the specs to run properly

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -77,14 +77,14 @@ describe Commander::Command do
       
       it "calling the #call method by default when an object is called" do
         object = mock 'Object'
-        object.should_receive(:call).with(an_instance_of(Array), an_instance_of(Commander::Command::Options)).once
+        object.should_receive(:call).once
         @command.when_called object
         @command.run 'foo'        
       end
             
       it "calling an arbitrary method when an object is called" do
         object = mock 'Object'
-        object.should_receive(:foo).with(an_instance_of(Array), an_instance_of(Commander::Command::Options)).once
+        object.should_receive(:foo).once
         @command.when_called object, :foo
         @command.run 'foo'        
       end
