bd2735ac29a80f8d2fc3cd204a16a86796b51485
visionmedia <tj@vision-media.ca>
2009-04-06 23:04:36 -0700

    Auto-loading help formatter base

--- lib/commander/help_formatters.rb
+++ lib/commander/help_formatters.rb
@@ -1,8 +1,7 @@
 
-require 'commander/help_formatters/base'
-
 module Commander
   module HelpFormatter
+    autoload :Base, 'commander/help_formatters/base'
     autoload :Terminal, 'commander/help_formatters/terminal'
     autoload :TerminalCompact, 'commander/help_formatters/terminal_compact'
   end
