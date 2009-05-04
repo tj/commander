9a1062917f62defa2bd578b08de657594c8476e2
visionmedia <tj@vision-media.ca>
2009-05-04 11:45:26 -0700

    Only wrap when we are talking to a terminal

--- lib/commander/import.rb
+++ lib/commander/import.rb
@@ -1,4 +1,4 @@
 
 require 'commander'
-$terminal.wrap_at = HighLine::SystemExtensions.terminal_size.first - 5 rescue 80
+$terminal.wrap_at = HighLine::SystemExtensions.terminal_size.first - 5 rescue 80 if $stdin.tty?
 at_exit { run! }
