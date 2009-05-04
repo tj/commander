ead9a3fd8c016fa77ff9e83d5ab1aec0c719e36f
visionmedia <tj@vision-media.ca>
2009-05-04 10:57:48 -0700

    Commander::UI.replace_tokens dup/gsub!

--- spec/ui_spec.rb
+++ spec/ui_spec.rb
@@ -3,15 +3,8 @@ describe Commander::UI do
   
   describe ".replace_tokens" do
     it "should replace tokens within a string, with hash values" do
-      result = Commander::UI.replace_tokens 'Welcome :name, enjoy your :object', :name => 'TJ', :object => 'cookie'
+      result = Commander::UI.replace_tokens 'Welcome :name, enjoy your :object'.freeze, :name => 'TJ', :object => 'cookie'
       result.should == 'Welcome TJ, enjoy your cookie'
-    end
-    
-    it "should not mutate the string passed" do
-      string = 'Oh I will enjoy my :object'
-      result = Commander::UI.replace_tokens string, :object => 'cookies'
-      result.should == 'Oh I will enjoy my cookies'
-      string.should == 'Oh I will enjoy my :object'
     end
   end
 
