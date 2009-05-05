9afbf1b4309a2e6bd7336e87ae135ebaa57025f1
visionmedia <tj@vision-media.ca>
2009-05-05 10:13:09 -0700

    Removed delegations from Object

--- lib/commander/core_ext/object.rb
+++ lib/commander/core_ext/object.rb
@@ -1,35 +1,5 @@
 
-require 'forwardable'
-
-##
-# Delegates the following methods:
-#
-# * Commander::Runner#add_command
-# * Commander::Runner#command
-# * Commander::Runner#commands
-# * Commander::Runner#program
-# * Commander::UI::ProgressBar#progress
-#
-
 class Object
-  
-  extend Forwardable
-  include Commander::UI
-  include Commander::UI::AskForClass
-  
-  #--
-  # UI related delegation.
-  #++
-  
-  def_delegators Commander::UI::ProgressBar, :progress
-  
-  #--
-  # Command runner delegation.
-  #++
-  
-  def_delegators :'Commander::Runner.instance', 
-    :add_command, :command, :program, :run!, :global_option,
-    :commands, :alias_command, :default_command
 
   ##
   # Return the current binding.
