be8ce4a6980f1f7d7bfab9b11ce6c736311ae240
visionmedia <tj@vision-media.ca>
2009-05-05 10:58:21 -0700

    Moving things around

--- lib/commander/import.rb
+++ lib/commander/import.rb
@@ -2,9 +2,9 @@
 require 'commander'
 require 'commander/delegates'
 
-$terminal.wrap_at = HighLine::SystemExtensions.terminal_size.first - 5 rescue 80 if $stdin.tty?
-at_exit { run! }
-
 include Commander::UI
 include Commander::UI::AskForClass
 include Commander::Delegates
+
+$terminal.wrap_at = HighLine::SystemExtensions.terminal_size.first - 5 rescue 80 if $stdin.tty?
+at_exit { run! }
