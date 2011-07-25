de5c7a18d8582f5cfa10962af42c5794d5d2f149
Justin Lynn <j@3crowd.com>
2011-07-25 00:01:05 -0700

    fix platform test, was invalid due to order of evaluation

--- lib/commander/platform.rb
+++ lib/commander/platform.rb
@@ -2,7 +2,7 @@
 module Commander
   module Platform
     def self.jruby?
-      defined? RUBY_ENGINE && RUBY_ENGINE=='jruby'
+      defined?(RUBY_ENGINE) && (RUBY_ENGINE == 'jruby')
     end
   end
 end
