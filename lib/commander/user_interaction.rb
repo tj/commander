bb207cf71a53d870b5fa478dff2bfc0e79d53bf9
Gabriel Gilder <ggilder@tractionco.com>
2012-02-16 14:39:39 -0800

    remove unnecessary io redirection

--- lib/commander/user_interaction.rb
+++ lib/commander/user_interaction.rb
@@ -257,7 +257,7 @@ module Commander
       begin
         tmpfile.write input if input
         tmpfile.close
-        system("#{editor} #{tmpfile.path.shellescape} < `tty` > `tty`") ? IO.read(tmpfile.path) : nil
+        system("#{editor} #{tmpfile.path.shellescape}") ? IO.read(tmpfile.path) : nil
       ensure
         tmpfile.unlink
       end
