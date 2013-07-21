9ea5c0f943d4df79cbbda01b0486d9333269aedd
Gabriel Gilder <gabriel@gabrielgilder.com>
2013-07-20 19:50:17 -0700

    Format multiline program and command descriptions
    Apply indentation to multiline program and command descriptions.
    Fixes #54.

--- lib/commander/help_formatters.rb
+++ lib/commander/help_formatters.rb
@@ -4,5 +4,10 @@ module Commander
     autoload :Base, 'commander/help_formatters/base'
     autoload :Terminal, 'commander/help_formatters/terminal'
     autoload :TerminalCompact, 'commander/help_formatters/terminal_compact'
+
+    module_function
+    def indent amount, text
+      text.gsub("\n", "\n" + (' ' * amount))
+    end
   end
 end
