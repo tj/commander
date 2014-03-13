a20668c13e4f7ea633e251fc0cdc8e6a9513f473
krissi <christianhaase@ak-palme.de>
2014-03-13 10:08:38 +0100

    renamed Commander::Mixin in block.rb

--- lib/commander/block.rb
+++ lib/commander/block.rb
@@ -1,12 +1,12 @@
 require 'commander'
-require 'commander/mixin'
+require 'commander/methods'
 
 module Commander
   module_function
 
   def configure(*configuration_opts, &configuration_block)
     configuration_module = Module.new
-    configuration_module.extend Commander::Mixin
+    configuration_module.extend Commander::Methods
     
     configuration_module.class_exec(*configuration_opts, &configuration_block)
 
